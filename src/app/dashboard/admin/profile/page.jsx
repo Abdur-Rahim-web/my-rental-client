a656c561bd3034d7a29d8d434053d356041786cd
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-06-26 22:29:07 +0600

    feat: added admin profile page

---
+++ src/app/dashboard/admin/profile/page.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import UserInfoCard from "@/components/dashboard/UserInfoCard";
+
+const ProfilePage = () => {
+    return (
+        
+        <div className="flex justify-center items-center bg-gray-100 h-[calc(100vh-80px)] w-full">
+            <UserInfoCard />
+        </div>
+    );
+};
+
+export default ProfilePage;
